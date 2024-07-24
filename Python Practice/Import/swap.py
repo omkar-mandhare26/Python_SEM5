1e6f010034debcc58fb3cc9e837abbff55833670
Omkar Mandhare <omkarmandhare530@gmail.com>
2024-07-24 22:09:42 +0530

    For Backup

---
+++ Python Practice/Import/swap.py
@@ -0,0 +1,12 @@
+a = 2
+b = 4
+
+print(f"Original value of a: {a}")
+print(f"Original value of b: {b}")
+
+temp = a
+a = b
+b = temp
+
+print(f"Swapped value of a: {a}")
+print(f"Swapped value of b: {b}")
